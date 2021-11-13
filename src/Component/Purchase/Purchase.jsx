1a84cec1181796661731139988db5f6495a776d8
Mohammad Ali Sakib <mohammad.ali.sakib.pk@gmail.com>
2021-11-13 23:43:09 +0600

    product page show product item

--- src/Component/Purchase/Purchase.jsx
+++ src/Component/Purchase/Purchase.jsx
@@ -39,7 +39,36 @@ const Purchase = () => {
         
     }
     return (
+        <>
+        
         <div className="p-5 bg-gray-700 ">
+            <div className="p-10">
+                <div className=" w-full lg:max-w-full lg:flex">
+                <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style={{backgroundImage: `url("${post?.photo}")`}} title="M">
+                </div>
+                <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                    <div className="mb-8">
+                    <p className="text-sm text-gray-600 flex items-center">
+                        <svg className="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
+                        </svg>
+                        Order Product only
+                    </p>
+                    <div className="text-gray-900 font-bold text-xl mb-2">{post?.product}</div>
+                    <p className="text-gray-700 text-base">{post?.description}</p>
+                    </div>
+                    <div className="flex items-center">
+                    <img className="w-10 h-10 rounded-full mr-4" src={user?.photoURL} alt="A"/>
+                    <div className="text-sm">
+                        <p className="text-gray-900 leading-none">{user?.displayName}</p>
+                        <p className="text-gray-600">Recent Now</p>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+            {/* Card End */}
             <div className="mt-8 p-4">
                 <form onSubmit={handleSubmit(onSubmit)}>
                     
@@ -99,6 +128,7 @@ const Purchase = () => {
                 </form>
             </div>
         </div>
+        </>
     )
 }
 
