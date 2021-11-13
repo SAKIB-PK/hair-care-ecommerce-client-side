233adf40229f4164a0ff7f1f1a35e6067afd73d5
Mohammad Ali Sakib <mohammad.ali.sakib.pk@gmail.com>
2021-11-13 20:33:07 +0600

    google login problem solved

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -51,7 +51,7 @@ const Login = () => {
                                     <h6 className="text-gray-600 text-sm font-bold">Sign in with</h6></div>
                                 <div className="btn-wrapper text-center">
                                     <button className="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center  text-xs" type="button" style={{transition: "all 0.15s ease 0s"}}><img alt="..." className="w-5 mr-1" src="images/icons-github.svg"/>Github</button>
-                                    <button className="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center  text-xs" type="button" style={{transition: "all 0.15s ease 0s"}}><img alt="..." className="w-5 mr-1" src="images/icons-google.svg" onClick={hundleGoogle}/>Google</button>
+                                    <button className="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center  text-xs" type="button" style={{transition: "all 0.15s ease 0s"}} onClick={hundleGoogle}><img alt="..." className="w-5 mr-1" src="images/icons-google.svg" />Google</button>
                                 </div>
                                 <hr className="mt-6 border-b-1 border-gray-400"/>
                             </div>
